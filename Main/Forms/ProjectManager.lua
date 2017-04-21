488c4676ab360e94dd7dd6b458a3f8d2413e3bae
Tricky1975 <facebook@tbbs.nl>
2017-04-21 17:59:32 +0200

    Set up project creation tab

--- Main/Forms/ProjectManager.lua
+++ Main/Forms/ProjectManager.lua
@@ -6,3 +6,23 @@ function FORM_ProjectManager_Close()
      MAAN_SaveVar(projectlist,"ProjectList.lua")
      os.exit()
 end     
+
+
+function OwnDirAllowed(destroy)
+	MAAN_Enabled('KID_TEXTFIELD_ProjectCreateDir',MAAN_Checked('KID_RADIO_CreateIn#CustomDir'))
+	-- --[[
+	if MAAN_Checked('KID_RADIO_CreateIn#DefaultDir') then
+		MAAN_Text('KID_TEXTFIELD_ProjectCreateDir',"$Documents$/LAURA2Projects/"..MAAN_Text('KID_TEXTFIELD_Title'))
+	elseif destroy then
+		MAAN_Text('KID_TEXTFIELD_ProjectCreateDir',"")
+	end
+	-- ]]
+end; OwnDirAllowed()
+
+function KID_RADIO_CreateIn_Action(idx)
+	OwnDirAllowed(true)
+end	
+
+function KID_TEXTFIELD_Title_Action(idx)
+	OwnDirAllowed()
+end	
