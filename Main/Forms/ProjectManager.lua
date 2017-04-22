5417ff921a9f29892fe2a045807e3b287591faf5
Tricky1975 <facebook@tbbs.nl>
2017-04-22 14:07:25 +0200

    Title checkup for creation

--- Main/Forms/ProjectManager.lua
+++ Main/Forms/ProjectManager.lua
@@ -27,6 +27,23 @@ function KID_TEXTFIELD_Title_Action(idx)
 	OwnDirAllowed()
 end	
 
+function KID_BUTTON_CreateProject_Action()
+   local title = MAAN_Text('KID_TEXTFIELD_Title'):gsub("^%s*(.-)%s*$", "%1")
+   -- If there is no title at all then no go!
+   if title=="" then return alert("Sorry, I do need a title before I can create a project for you!") end
+   -- Let's now see if the name can be allowed
+   local allowed = "qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm 1234567890_-"
+   allowstring = true  
+   for i1=1,len(title) do
+       allowchar = false 
+       for i2=1,len(allowed) do
+            allowchar = allowchar or (title:sub(i1,i1)==allowed:sub(i2,i2))
+       end
+       allowstring = allowstring and allowchar
+   end
+   if not allowstring then return alert("Sorry!\nI cannot accept that title.\nI only allow roman letters WITHOUT accepts or umlauts, spaces, numbers, dashes and underscores") end     
+end
+
 function GALE_OnLoad()
   CSay("Script for project Manager loaded -- Configuring....")
 end  
